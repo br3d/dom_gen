4191e0acc1d4725499ece21051269219ad270bce
br3d <13rihon@gmail.com>
2014-07-04 12:27:53 +0400

    add exclude -

--- gen.py
+++ gen.py
@@ -2,9 +2,9 @@ import itertools
 str =''
 num=2
 all=[]
-mas = ['a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+mas = ['-','a','b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 for n in itertools.product(mas, repeat=3):
-    all.append(''.join(n)+'.ru')
+    if ((n[0] != '-') and (n[num] != '-')): all.append(''.join(n)+'.ru')
 #print all
 #
 head = open("name.txt", "a+")
