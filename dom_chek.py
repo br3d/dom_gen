b0f5598effc07139997e74b86fb922fbe9ccfdbf
br3d <br3d@loki.rostovoblgaz.ru>
2014-07-07 13:16:46 +0400

    add threading

--- dom_chek.py
+++ dom_chek.py
@@ -1,16 +1,47 @@
 import whois, time
+from threading import Thread
+from Queue import Queue
 
-dm_list = open("name_w.txt").read().split('\n')
+dm_list = open("name.txt").read().split('\n')
 good = open("good.txt", "a+")
-for x in(dm_list):
-    time.sleep(1)
-    print 'check - ',x
-    domain = whois.query(x)
-    try:
-        domain.name
-    except Exception, detail:
-        print "Domain %s non occupied" %x
-	good.write(x+"\n")
-    #return 0
+thread_num = 5
+q = Queue()
+
+def dom_check():
+	for x in(dm_list):
+	    time.sleep(1)
+	    print 'check - ',x
+	    domain = whois.query(x)
+	    try:
+	        domain.name
+	    except Exception, detail:
+	        print "Domain %s non occupied" %x
+		good.write(x+"\n")
+	    #return 0
+
+def dom_chk(x):
+	time.sleep(1)
+	print 'check - ',x
+	domain = whois.query(x)
+	try:
+	    domain.name
+	except Exception, detail:
+	    print "Domain %s non occupied" %x
+	    good.write(x+"\n")
+
+def worker():
+	while True:
+		item = q.get()
+		dom_chk(item)
+		q.task_done()
 
 
+for i in range (thread_num):
+	t = Thread(target=worker)
+	t.setDaemon(True)
+	t.start()
+
+for item in dm_list:
+	q.put(item)
+
+q.join()
